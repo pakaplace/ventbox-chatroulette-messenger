1b9bf21ee184863f252f19ffced242004e2163bb
pakaplace <pleeplace@gmail.com>
2017-03-28 22:29:30 +0800

    added config vars to client side by passing through express route

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  res.render('index', { title: 'Ventbox' });
+  res.render('index', { title: 'Ventbox', port: process.env.PORT });
 });
 
 module.exports = router;
